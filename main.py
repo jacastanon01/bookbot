65b765b16695d2c0aabe985768d1ffb5ad544aa8
Jacob Castanon <jacastanon01@gmail.com>
2024-02-18 03:24:34 -0600

    finished report calculations and printed each sorted letter by number

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class Book():
         self.filename = filename
         self.letters_dict = {}
 
-    def __raise_if_invalid_file(filename):
+    def __raise_if_invalid_file(self, filename):
         if filename[-3:] != "txt":
             raise ValueError("only accepts txt files")
 
@@ -31,9 +31,30 @@ class Book():
                     self.letters_dict[c] += 1              
         return self.letters_dict
 
+def get_char_num(dict):
+    return dict["count"]
+
 def print_report(book):
-    print('-- Processing franketstein --')
-    print(book.filename)
+    char_dict = book.get_letter_count()
+    char_report_list = []
+    word_count = book.get_word_count()
+    title = f"-- Processing file `{book.filename}` --"
+    print(title)
+    # print ('-' for i in range(len(title)))
+
+    for (key,value) in char_dict.items():
+        items = {}
+        items["key"] = key
+        items["count"] = value
+        char_report_list.append(items)
+        char_report_list.sort(reverse=True, key=get_char_num)
+
+    # print(f"{word_count} words found!\nHere is an analysis of the most used letters:")
+
+
+    for letter in char_report_list:
+        print(f"'{letter['key'].upper()}' was found {letter['count']} times")
+
 
 def main():
     if len(argv) != 2:
