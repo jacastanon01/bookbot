7740e93f63a14ba30e92d8da830ec9f390e6425b
Jacob Castanon <jacastanon01@gmail.com>
2024-02-18 00:42:42 -0600

    added error handling for cli input

--- main.py
+++ main.py
@@ -1,39 +1,46 @@
+from sys import argv
+
 class Book():
     def __init__(self, filename):
         self.file_text = ""
         self.filename = filename
         self.letters_dict = {}
 
-    def read_file(self):
-        file_length = len(self.filename)
-        if self.filename[-3:] != "txt":
+    def __raise_if_invalid_file(filename):
+        if filename[-3:] != "txt":
             raise ValueError("only accepts txt files")
+
+    def read_file(self):
+        self.__raise_if_invalid_file(self.filename)
 
         with (open(self.filename) as f):
             self.file_text = f.read()
 
-    def count_words(self):
+    def get_word_count(self):
         return len(self.file_text.split())
 
-    def count_letters(self):
+    def get_letter_count(self):
         self.read_file()
-        lower_text = self.file_text.lower()
-        for c in lower_text[:100]:
+        char = self.file_text.lower()
+        for c in char[:100]:
             if 96 < ord(c) < 122:
                 letter = self.letters_dict.get(c)
                 if letter is None:
                     self.letters_dict[c] = 1
                 else:
-                    self.letters_dict[c] += 1
-                
-        print(self.letters_dict)
+                    self.letters_dict[c] += 1              
+        return self.letters_dict
 
+def print_report(book):
+    print('-- Processing franketstein --')
+    print(book.filename)
 
 def main():
-    book = Book("../books/frankenstein.txt")
-    book.count_letters()
-    # print(book.count_words())
-    # print(book.file_text[:100])
+    if len(argv) != 2:
+        raise ValueError("expecting a file to read")
+    book_file = argv[1]
+    book = Book(book_file)
+    print_report(book)
         
 if __name__ == "__main__":
     main()
